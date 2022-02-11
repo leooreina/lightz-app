771484c29f8f9960a1c2432d72f8ba922ce9948f
Leonardo Reina <leo.reina01@gmail.com>
2022-02-11 17:35:22 -0300

    list work properly

--- lib/widgets/where_to_go_widget.dart
+++ lib/widgets/where_to_go_widget.dart
@@ -24,7 +24,9 @@ class _WhereToGoState extends State<WhereToGo> {
         description: 'Pubs',
         url:
             'https://images.unsplash.com/photo-1593887937265-2a09787dcc19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80'),
-    Subtitle(description: 'Lounge', url: 'https://placeimg.com/640/480/any')
+    Subtitle(
+        description: 'Lounge',
+        url: 'https://placeimg.com/640/480/any')
   ];
 
   @override
@@ -33,17 +35,17 @@ class _WhereToGoState extends State<WhereToGo> {
       height: 120,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
+        itemCount: subtitles.length,
         itemBuilder: (context, index) {
           return Container(
             margin: const EdgeInsets.symmetric(horizontal: 6.0),
-            // color: Colors.tealAccent,
             child: Column(
               children: [
                 Card(
                   semanticContainer: true,
                   clipBehavior: Clip.antiAliasWithSaveLayer,
                   child: Image.network(
-                    'https://placeimg.com/640/480/any',
+                    subtitles[index].url,
                     height: 80,
                     width: 90,
                     fit: BoxFit.fill,
@@ -54,76 +56,13 @@ class _WhereToGoState extends State<WhereToGo> {
                   elevation: 5,
                   margin: EdgeInsets.all(10),
                 ),
-                Text('$index')
-                // ...subtitles.map((product) {
-                //   return Text(product.description);
-                // }).toList(),
+                Text(subtitles[index].description)
               ],
             ),
           );
         },
       ),
     );
-    // return SizedBox(
-    //   height: 100,
-    //   child: ListView(
-    //     scrollDirection: Axis.horizontal,
-    //     children: [
-    //       Card(
-    //         semanticContainer: true,
-    //         clipBehavior: Clip.antiAliasWithSaveLayer,
-    //         child: Image.network(
-    //           'https://cdn.pixabay.com/photo/2018/07/14/15/27/cafe-3537801_1280.jpg',
-    //           fit: BoxFit.cover,
-    //         ),
-    //         shape: RoundedRectangleBorder(
-    //           borderRadius: BorderRadius.circular(10.0),
-    //         ),
-    //         elevation: 5,
-    //         margin: EdgeInsets.all(10),
-    //       ),
-    //       Card(
-    //         semanticContainer: true,
-    //         clipBehavior: Clip.antiAliasWithSaveLayer,
-    //         child: Image.network(
-    //           'https://cdn.pixabay.com/photo/2016/11/18/14/05/brick-wall-1834784_1280.jpg',
-    //           fit: BoxFit.fill,
-    //         ),
-    //         shape: RoundedRectangleBorder(
-    //           borderRadius: BorderRadius.circular(10.0),
-    //         ),
-    //         elevation: 5,
-    //         margin: EdgeInsets.all(10),
-    //       ),
-    //       Card(
-    //         semanticContainer: true,
-    //         clipBehavior: Clip.antiAliasWithSaveLayer,
-    //         child: Image.network(
-    //           'https://cdn.pixabay.com/photo/2016/11/18/22/21/restaurant-1837150_1280.jpg',
-    //           fit: BoxFit.fill,
-    //         ),
-    //         shape: RoundedRectangleBorder(
-    //           borderRadius: BorderRadius.circular(10.0),
-    //         ),
-    //         elevation: 5,
-    //         margin: EdgeInsets.all(10),
-    //       ),
-    //       Card(
-    //         semanticContainer: true,
-    //         clipBehavior: Clip.antiAliasWithSaveLayer,
-    //         child: Image.network(
-    //           'https://cdn.pixabay.com/photo/2013/11/12/01/29/bar-209148_1280.jpg',
-    //           fit: BoxFit.fill,
-    //         ),
-    //         shape: RoundedRectangleBorder(
-    //           borderRadius: BorderRadius.circular(10.0),
-    //         ),
-    //         elevation: 5,
-    //         margin: EdgeInsets.all(10),
-    //       )
-    //     ],
-    //   ),
-    // );
   }
 }
 
